5d008b6a1b26f4cd4c88c9e0ca90942e0e0688de
FarrukhS <909tjk@gmail.com>
2024-10-26 20:32:04 -0500

    added main section and about sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import About from "./constants/About"
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+      <div className="fixed top-0 -z-10 h-full w-full">
+        <div className="relative h-full w-full bg-slate-950"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#3e3e3e,transparent)]"></div></div>
+      </div>
+      
+      <div className="container mx-auto px-8">
+        <Navbar />
+        <Hero />
+        <About />
+      </div>
+      
     </div>
   )
 }
