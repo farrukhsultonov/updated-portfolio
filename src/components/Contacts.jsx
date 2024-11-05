e6636b8fed0fac725c2f031e41d0df78a186649c
FarrukhS <909tjk@gmail.com>
2024-11-05 07:50:05 -0600

    completed animation

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,15 +1,24 @@
 import { CONTACT } from "../constants";
+import { motion } from "framer-motion"
 
 const Contacts = () => {
   return (
     <div className="border-b border-neutral-900 pb-20">
-      <h2 className="my-10 text-center text-4xl">Get in Touch</h2>
-      <div className="text-center tracking-tighter">
+      <motion.h2 
+      whileInView={{ opacity: 1, y: 0 }}
+      initial={{ opacity: 0, y: -100 }}
+      transition={{ duration: 0.5 }}
+      className="my-10 text-center text-4xl">Get in Touch</motion.h2>
+      <motion.div 
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: 100 }}
+        transition={{ duration: 1 }}
+      className="text-center tracking-tighter">
         <a href="https://www.linkedin.com/in/farrukh-sultonov/" target="_blank" className="border-b">{CONTACT.linkedIn}</a>
         <br />
         <br />
         <a href="#" className="border-b">{CONTACT.email}</a>
-      </div>
+      </motion.div>
     </div>
   )
 }
