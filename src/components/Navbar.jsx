d583236f0c74d02a9ae47a73b30aea43e34aaa7e
FarrukhS <909tjk@gmail.com>
2024-11-28 14:52:44 -0600

    added resume download button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
-import logo from "../assets/logo.png"
-import { FaLinkedin } from "react-icons/fa"
-import { FaGithub } from "react-icons/fa"
+import logo from "../assets/logo.png";
+import { FaLinkedin, FaGithub, FaDownload } from "react-icons/fa";
 
 const Navbar = () => {
   return (
@@ -8,12 +7,27 @@ const Navbar = () => {
       <div className="flex flex-shrink-0 items-center">
         <img className="mx-2 w-14" src={logo} alt="logo" />
       </div>
+      
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <a href="https://www.linkedin.com/in/farrukh-sultonov/" target="_blank"><FaLinkedin /></a>
-        <a href="https://github.com/farrukhsultonov" target="_blank"><FaGithub /></a>
+        <a href="https://www.linkedin.com/in/farrukh-sultonov/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/farrukhsultonov" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a
+          href="https://drive.google.com/file/d/19i5VhcGBoB8fWz9it5eWksFGho9QCnDR/view?usp=sharing"
+          download="Farrukh_Sultonov_Resume.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center text-lg gap-1  hover:text-blue-300"
+        >
+          <FaDownload />
+          <span>Resume</span>
+        </a>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
